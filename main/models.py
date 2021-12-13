4e0158416e73d735dad189e945a42154255277b4
yekk11 <08021382121014.kmmi@student.unsri.ac.id>
2021-12-13 18:59:53 +0700

    rev13122021

--- main/models.py
+++ main/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Mahasiswa(models.Model):
     nama = models.CharField(max_length=50)
-    nim = models.CharField(max_length=20)
+    nim = models.CharField(max_length=20,primary_key=True)
     email = models.EmailField(max_length=100)
 
     def __str__(self):
